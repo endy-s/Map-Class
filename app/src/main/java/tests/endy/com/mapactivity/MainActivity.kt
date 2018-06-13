ae94033f12bb11aa3226f842a4f20fe47f4c05f2
Endy Silveira <endy.silveira@gmail.com>
2018-06-13 00:50:26 -0300

    Step 2: UI - Add the MapView to the Layout

--- app/src/main/java/tests/endy/com/mapactivity/MainActivity.kt
+++ app/src/main/java/tests/endy/com/mapactivity/MainActivity.kt
@@ -12,9 +12,12 @@ class MainActivity: AppCompatActivity() {
         // 1.2. Create and add it to the google_maps_api.xml at values folder
         // 1.3. Add to the Manifest the map key info using the meta-data tag
 
+    // 2. UI
+        // 2.1 Add the MapView to your layout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_map)
     }
 
 }
