325b529ad0d0b9ac117ea03a6c77010a8e9a8b9e
Endy Silveira <endy.silveira@gmail.com>
2018-06-12 23:57:22 -0300

    Step 1: Map API Key

--- app/src/main/java/tests/endy/com/mapactivity/MainActivity.kt
+++ app/src/main/java/tests/endy/com/mapactivity/MainActivity.kt
@@ -5,6 +5,13 @@ import android.support.v7.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
 
+    // -- Map --
+
+    // 1. API Key
+        // 1.1. Create an Map API key at https://developers.google.com/maps/documentation/android/signup
+        // 1.2. Create and add it to the google_maps_api.xml at values folder
+        // 1.3. Add to the Manifest the map key info using the meta-data tag
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
